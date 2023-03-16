8a319bbaf2009952e8ac7fbaf65315a4a82e4a3a
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:43:44 +0100

    Fix tabs and line ends

--- java-embedded-compiler-itest/src/test/resources/compile1/rest/northwind/services/OrderLink.java
+++ java-embedded-compiler-itest/src/test/resources/compile1/rest/northwind/services/OrderLink.java
@@ -9,9 +9,9 @@ package rest.northwind.services;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,22 +28,22 @@ import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
 public class OrderLink {
-	// OSGI service reference, the class exported by another bundle
-	LogService logService;
+    // OSGI service reference, the class exported by another bundle
+    LogService logService;
 
-	// Getters
+    // Getters
 
-	// Setters
+    // Setters
 
-	public Map<String, Object> toMap() {
-		Map ret = new LinkedHashMap(); 
-		return ret;
-	}
+    public Map<String, Object> toMap() {
+        Map ret = new LinkedHashMap();
+        return ret;
+    }
 
-	public static OrderLink fromMap(Map<String, Object> map) {
-		OrderLink ret = new OrderLink();
+    public static OrderLink fromMap(Map<String, Object> map) {
+        OrderLink ret = new OrderLink();
 
-	    return ret;		
-	}
+        return ret;
+    }
 
 }
